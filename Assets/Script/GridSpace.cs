f1e5a6ed60c64d510b3fc21fa8a8bcee6a5b6363
LAPTOP-78QD76RI\User <pannawish.hir63@dome.tu.ac.th>
2021-09-05 11:35:02 +0700

    Not finish

---
+++ Assets/Script/GridSpace.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GridSpace : MonoBehaviour
+{
+    public Button button;
+    public Text buttontext;
+    public string playerSide;
+
+    private GameController gameController;
+
+    public void setGameControllerReference (GameController controller)
+    {
+        gameController = controller;
+    }
+    public void SetSpace()
+    {
+        buttontext.text = playerSide;
+        button.interactable = false;
+    }
+}
